0ffc6bf7b97e44cb78c9d41336c4fe621acf00f0
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-09-05 14:30:53 +0200

    Student osztály tulajdonságai

--- StudentMVVMProjekt/Models/Student.cs
+++ StudentMVVMProjekt/Models/Student.cs
@@ -1,6 +1,18 @@
-﻿namespace StudentMVVMProjekt.Models
+﻿using Microsoft.VisualBasic;
+using System.Collections.Generic;
+
+namespace StudentMVVMProjekt.Models
 {
+    public enum SchoolClassType { CLASS_A, CLASS_B, CLASS_C }
     public class Student
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateAndTime BirthsDay { get; set; }
+        public int Year { get; set; }
+        public SchoolClassType SchoolClass { get; set; }
+        public List<string> EducationLevel { get; set; }
+
+
     }
 }
