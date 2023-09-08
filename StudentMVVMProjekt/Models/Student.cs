b5648bac0ff6870885fb0eefc5e3959c3d2fd772
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-09-08 11:06:06 +0200

    Student osztály

--- StudentMVVMProjekt/Models/Student.cs
+++ StudentMVVMProjekt/Models/Student.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 
 namespace StudentMVVMProjekt.Models
@@ -6,13 +7,31 @@ namespace StudentMVVMProjekt.Models
     public enum SchoolClassType { CLASS_A, CLASS_B, CLASS_C }
     public class Student
     {
+        public Student()
+        {
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            BirthsDay = new DateTime();
+            Year = 9;
+            SchoolClass = SchoolClassType.CLASS_A;
+            EducationLevel = new List<string>();
+        }
+
+        public Student(string firstName, string lastName, DateTime birthsDay, int year, SchoolClassType schoolClass, List<string> educationLevel)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthsDay = birthsDay;
+            Year = year;
+            SchoolClass = schoolClass;
+            EducationLevel = educationLevel;
+        }
+
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public DateAndTime BirthsDay { get; set; }
+        public DateTime BirthsDay { get; set; }
         public int Year { get; set; }
         public SchoolClassType SchoolClass { get; set; }
-        public List<string> EducationLevel { get; set; }
-
-
+        public List<string> EducationLevel { get; set; }       
     }
 }
